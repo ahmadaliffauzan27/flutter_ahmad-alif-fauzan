0d8305cda4587f6e2e4956c95056765c777d44ed
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-02-21 14:26:54 +0700

    add projek dart section 7

---
+++ 7_Branching-Looping-Function/Pratikum/pratikum_section7/bin/pratikum_section7.dart
@@ -0,0 +1,5 @@
+import 'package:pratikum_section7/pratikum_section7.dart' as pratikum_section7;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pratikum_section7.calculate()}!');
+}
---
+++ 7_Branching-Looping-Function/Pratikum/pratikum_section7/test/pratikum_section7_test.dart
@@ -0,0 +1,8 @@
+import 'package:pratikum_section7/pratikum_section7.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
