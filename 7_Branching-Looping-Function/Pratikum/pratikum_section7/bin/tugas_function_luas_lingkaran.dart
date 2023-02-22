0c557710e1a51445c56cd33c88fc6c9fe115c4c0
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-02-22 15:56:30 +0700

    add tugas function menghitung luas lingkaran to pratikum section 7

---
+++ 7_Branching-Looping-Function/Pratikum/pratikum_section7/bin/tugas_function_luas_lingkaran.dart
@@ -0,0 +1,9 @@
+void luasLingkaran(){
+  double jariJari = 7;
+  double luas = 3.14 * jariJari * jariJari;
+  print('Luas lingkaran dengan jari-jari $jariJari adalah: $luas');
+}
+
+void main(){
+  luasLingkaran();
+}
