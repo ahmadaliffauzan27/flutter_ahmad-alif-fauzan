cf0f5bf85ab6a4f3f2d6cd92afb280a4297e7841
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-02-26 08:01:18 +0700

    add soal eksplorasi tabel perkalian

--- 7_Branching-Looping-Function/Pratikum/pratikum_section7/bin/soal_eksplorasi_tabel_perkalian.dart
+++ 7_Branching-Looping-Function/Pratikum/pratikum_section7/bin/soal_eksplorasi_tabel_perkalian.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 void main() {
-  for (int i = 1; i <= 9; i++) {
-    for (int j = 1; j <= 9; j++) {
+  stdout.write('Masukkan jumlah kolom dan baris: ');
+  int num = int.parse(stdin.readLineSync()!);
+
+  for (int i = 1; i <= num; i++) {
+    for (int j = 1; j <= num; j++) {
       // Menggunakan padLeft untuk menambahkan spasi pada kolom
       String value = (i * j).toString().padLeft(2);
       stdout.write('$value ');
