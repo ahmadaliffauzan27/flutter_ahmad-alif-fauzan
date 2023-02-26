9a2fa779d099d32c486128f9d3e43d075ae6975d
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-02-26 07:58:01 +0700

    add soal eksplorasi bilangan prima

--- 7_Branching-Looping-Function/Pratikum/pratikum_section7/bin/soal_eksplorasi_bilangan_prima.dart
+++ 7_Branching-Looping-Function/Pratikum/pratikum_section7/bin/soal_eksplorasi_bilangan_prima.dart
@@ -1,23 +1,51 @@
+
+import 'dart:io';
 void main() {
-  int num = 12;
+  stdout.write('Masukkan sebuah bilangan: ');
+  int number = int.parse(stdin.readLineSync()!);
   
-  if (isPrime(num)) {
-    print("$num adalah bilangan prima");
+  if (isPrime(number)) {
+    print('Bilangan $number adalah bilangan prima');
   } else {
-    print("$num bukan bilangan prima");
+    print('Bilangan $number bukan bilangan prima');
   }
 }
 
-bool isPrime(int num) {
-  if (num <= 1) {
+bool isPrime(int number) {
+  if (number <= 1) {
     return false;
   }
   
-  for (int i = 2; i <= num / 2; i++) {
-    if (num % i == 0) {
+  for (int i = 2; i <= number / 2; i++) {
+    if (number % i == 0) {
       return false;
     }
   }
   
   return true;
 }
+
+
+// void main() {
+//   int num = 12;
+  
+//   if (isPrime(num)) {
+//     print("$num adalah bilangan prima");
+//   } else {
+//     print("$num bukan bilangan prima");
+//   }
+// }
+
+// bool isPrime(int num) {
+//   if (num <= 1) {
+//     return false;
+//   }
+  
+//   for (int i = 2; i <= num / 2; i++) {
+//     if (num % i == 0) {
+//       return false;
+//     }
+//   }
+  
+//   return true;
+// }
