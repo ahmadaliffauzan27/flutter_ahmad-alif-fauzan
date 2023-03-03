2f182ed3142d5ca9b58cb09a3b1809aa9262328b
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-03-03 09:57:36 +0700

    add soal eksplorasi 2

--- 8_Advance Function - Async-Await - Collection/Pratikum/pratikum_section_8/bin/soal_eksplorasi_2.dart
+++ 8_Advance Function - Async-Await - Collection/Pratikum/pratikum_section_8/bin/soal_eksplorasi_2.dart
@@ -6,6 +6,6 @@ void main() {
       .split(',')
       .fold(<String, int>{}, (map, element) => map..update(element, (value) => value + 1, ifAbsent: () => 1));
 
-  print('Hasil Frekuensi:');
+  print('Hasil Frekuensi :');
   freqMap.forEach((key, value) => print('$key: $value'));
 }
