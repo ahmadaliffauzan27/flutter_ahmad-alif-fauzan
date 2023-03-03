c657b40004deeb66925f6fe827028ad60ea7078e
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-03-03 09:24:10 +0700

    add soal eksplorasi 2

--- 8_Advance Function - Async-Await - Collection/Pratikum/pratikum_section_8/bin/soal_eksplorasi_2.dart
+++ 8_Advance Function - Async-Await - Collection/Pratikum/pratikum_section_8/bin/soal_eksplorasi_2.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Masukkan data (pisahkan dengan spasi): ");
+  stdout.write("Masukkan data *pisahkan dengan spasi*: ");
   final freqMap = stdin.readLineSync()!
       .split(' ')
       .fold(<String, int>{}, (map, element) => map..update(element, (value) => value + 1, ifAbsent: () => 1));
