66965fc7303f0fe6e901950c4ac6207b7302a1de
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-03-03 09:50:36 +0700

    add screenshoot soal eksplorasi 1

--- 8_Advance Function - Async-Await - Collection/Pratikum/pratikum_section_8/bin/soal_eksplorasi_1.dart
+++ 8_Advance Function - Async-Await - Collection/Pratikum/pratikum_section_8/bin/soal_eksplorasi_1.dart
@@ -2,17 +2,19 @@ import 'dart:io';
 
 void main() {
   // Minta input dari user
-  stdout.write("Masukkan data *pisahkan dengan spasi* : ");
+  stdout.write("Masukkan data (pisahkan dengan koma',') : ");
   String input = stdin.readLineSync()!;
   
   // Buat list dari input
-  List<String> dataList = input.split(" ");
+  List<String> dataList = input.split(", ");
   
   // Buat set dari gabungan semua list untuk menghilangkan nilai yang sama
   Set<String> dataSet = Set<String>.from(dataList);
   
   // Konversi kembali ke list dan urutkan
-  List<String> uniqueList = dataSet.toList()..sort();
+  List<String> uniqueList = dataSet.toList();
+  // List<String> uniqueList = dataSet.toList()..sort();
+
   
   // Cetak hasil
   print("Data unik: ${uniqueList.join(", ")}");
