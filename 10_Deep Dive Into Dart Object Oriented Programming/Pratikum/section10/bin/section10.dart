eeac4658750d8b187bb1cc72f65172c66faa5260
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-03-08 13:13:45 +0700

    add new project section 10

---
+++ 10_Deep Dive Into Dart Object Oriented Programming/Pratikum/section10/bin/section10.dart
@@ -0,0 +1,5 @@
+import 'package:section10/section10.dart' as section10;
+
+void main(List<String> arguments) {
+  print('Hello world: ${section10.calculate()}!');
+}
---
+++ 10_Deep Dive Into Dart Object Oriented Programming/Pratikum/section10/test/section10_test.dart
@@ -0,0 +1,8 @@
+import 'package:section10/section10.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
