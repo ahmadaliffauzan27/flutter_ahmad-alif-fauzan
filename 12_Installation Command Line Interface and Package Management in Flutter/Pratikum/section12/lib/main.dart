986c050ed4c28ca341e4a204cef42614509bb4ec
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-03-11 18:55:19 +0700

    add screenshoot soal prioritas 2

--- 12_Installation Command Line Interface and Package Management in Flutter/Pratikum/section12/lib/main.dart
+++ 12_Installation Command Line Interface and Package Management in Flutter/Pratikum/section12/lib/main.dart
@@ -13,58 +13,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Hallo Alterra',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'This is Google Fonts',
-              style: GoogleFonts.lato(),
+      home: Scaffold(
+        backgroundColor: Colors.cyan[50],
+        appBar: AppBar(
+          title: Text(
+            'Hallo Alterra',
+            style: GoogleFonts.poppins(
+              fontSize: 16,
+              fontWeight: FontWeight.bold
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+        body: Center(
+          child: Text(
+            'Nama Saya Ahmad Alif Fauzan',
+            style: GoogleFonts.poppins(
+              fontSize: 16,
+              fontWeight: FontWeight.w300,
+            ),
+            ),
+            ),
+          ),
+        );
+  }}
