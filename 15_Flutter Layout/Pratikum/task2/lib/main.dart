f2df1a9847d9db4d3565f03230d696f1eaee9ddc
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-03-15 03:34:29 +0700

    add task 2

---
+++ 15_Flutter Layout/Pratikum/task2/lib/main.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'GridView'
+          ),
+        ),
+        body : GridView.count(
+      crossAxisCount: 4,
+      children: const [
+        Icon(
+        Icons.blur_on,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+        Icons.addchart_outlined,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+        Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+        Icons.audiotrack,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+        Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+        Icons.audiotrack,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+        Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+        Icons.audiotrack,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+        Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+        Icons.audiotrack,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+        Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+        Icons.audiotrack,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+        Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+        Icons.audiotrack,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+        Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+        Icons.audiotrack,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+        Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+        Icons.audiotrack,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+        Icon(
+        Icons.favorite,
+        color: Colors.pink,
+        size: 24.0,
+        semanticLabel: 'Text to announce in accessibility modes',
+        ),
+        Icon(
+        Icons.audiotrack,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+        Icon(
+        Icons.audiotrack,
+        color: Colors.green,
+        size: 30.0,
+        ),
+        Icon(
+        Icons.beach_access,
+        color: Colors.blue,
+        size: 36.0,
+        ),
+      ],
+    ),
+      ),
+    );
+  }
+}
