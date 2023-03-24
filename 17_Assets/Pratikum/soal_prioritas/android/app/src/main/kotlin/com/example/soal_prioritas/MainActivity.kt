1e6a1b2b80f5a5f4c6334651d1bf33940ac35837
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-03-24 15:17:49 +0700

    edit theme.dart soal prioritas 1

--- 17_Assets/Pratikum/soal_prioritas/android/app/src/main/kotlin/com/example/soal_prioritas/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.soal_prioritas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 17_Assets/Pratikum/soal_prioritas1/lib/theme.dart
+++ 17_Assets/Pratikum/soal_prioritas1/lib/theme.dart
@@ -2,6 +2,7 @@ import 'package:supercharged/supercharged.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+//definisi color
 Color fillColorForm = "E7E0EC".toColor();
 Color primaryColor = "6750A4".toColor();
 Color textColor1 = "49454F".toColor();
