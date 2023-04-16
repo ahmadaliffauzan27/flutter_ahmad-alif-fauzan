edb8f81a27f7153e4b2effe2f55d5ff743e90a4d
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-04-17 01:25:16 +0700

    update

--- 27_Finite State Machine & Unit Testing/Pratikum/soal_finite_state/test/model/api/api_test.dart
+++ 27_Finite State Machine & Unit Testing/Pratikum/soal_finite_state/test/model/api/api_test.dart
@@ -1,23 +1,23 @@
-// import 'package:test/test.dart';
-// import 'package:tugas_finite_state/model/apis/food_apis.dart';
+// // import 'package:test/test.dart';
+// // import 'package:tugas_finite_state/model/apis/food_apis.dart';
+
+// // void main() {
+// //   test('get all foods', () async {
+// //     var foods = await ApiFood.getFood();
+// //     expect(foods.isNotEmpty, true);
+// //   });
+// // }
+
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:provider/provider.dart';
+// import 'package:tugas_finite_state/view_model/food_provider.dart';
 
 // void main() {
-//   test('get all foods', () async {
-//     var foods = await ApiFood.getFood();
-//     expect(foods.isNotEmpty, true);
+//   test('getFoodFromApi should return a list of foods', () async {
+//     final foodProvider = FoodProvider();
+
+//     await foodProvider.getFoodFromApi();
+
+//     expect(foodProvider.foodList.length, greaterThan(0));
 //   });
 // }
-
-import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
-import 'package:tugas_finite_state/view_model/food_provider.dart';
-
-void main() {
-  test('getFoodFromApi should return a list of foods', () async {
-    final foodProvider = FoodProvider();
-
-    await foodProvider.getFoodFromApi();
-
-    expect(foodProvider.foodList.length, greaterThan(0));
-  });
-}
